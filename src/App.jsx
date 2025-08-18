a6c6fc4a2ce8b48932d4e3e0c9541adc17893a1d
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-18 15:29:20 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { useReducer, useRef } from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import Diary from './pages/Diary'
 import Edit from './pages/Edit'
@@ -9,10 +10,50 @@ import New from './pages/New'
 import { getEmotionImage } from './util/getEmotionImage'
 import Header from './components/Header'
 import Button from './components/Button'
+
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용"
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용"
+  }
+]
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state]
+    default:
+      return state
+  }
+
+}
+
 function App() {
 
+  const [data, dispatch] = useReducer(reducer, mockData)
+  const idRef = useRef(3)
 
+  const onCreate = (createdDate, emotionId, content) => {
 
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content
+      }
+    })
+  }
   return (
     <div>
       <Header
@@ -21,7 +62,10 @@ function App() {
         rightChild={<Button text="right" />}
       />
 
-
+      <button onClick={() =>
+        onCreate(new Date().getTime(), 1, "hello")}>
+        일기 추가하기
+      </button>
 
       <Routes>
         <Route path='/' element={<Home />} />
