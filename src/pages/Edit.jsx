6e51dae9f93c7e9b85f458ad153cf2edb1947c27
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-21 16:23:10 +0900

    .

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -4,30 +4,13 @@ import Editor from '../components/Editor'
 import Header from '../components/Header'
 import Button from '../components/Button'
 import { DiaryDispatchContext, DiaryStateContext } from '../App'
+import useDiary from '../hook/useDiary'
 
 const Edit = () => {
-  const nav = useNavigate()
   const params = useParams()
   const { onDelete, onUpdate } = useContext(DiaryDispatchContext)
-  const data = useContext(DiaryStateContext)
-  const [curDiaryItem, setCurDiaryItem] = useState(null)
-
-  useEffect(() => {
-
-    const currentDiaryItem = data.find(
-      (item) => String(item.id) === String(params.id)
-    )
-    console.log(currentDiaryItem)
-    // if (!currentDiaryItem) {
-    //   window.alert('존재하지 않는 일기입니다.')
-    //   nav('/', { replace: true })
-    
-    // }
-    setCurDiaryItem(currentDiaryItem)
-
-  }, [params.id, data, nav])
-
-
+  const curDiaryItem = useDiary(params.id)
+  
   const onSubmit = (input) => {
     if (window.confirm('일기를 정말 수정할까요?')) {
       onUpdate(
